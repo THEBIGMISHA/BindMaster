68873e5fc20b2d0cfb5b4ff0860163dc0815b73d
Michael Nikolaev <esp12p@gmail.com>
2023-10-13 10:35:53 +0300

    13.10.23

--- INSTALLER_WIN.bat
+++ INSTALLER_WIN.bat
@@ -1,10 +1,3 @@
 @echo off
-copy %~dp0src\welcome.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
-cd "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\"
-mkdir BindMaster
-copy %~dp0src\main.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\"
-copy %~dp0src\slot1.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\"
-copy %~dp0src\slot2.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\"
-copy %~dp0src\slot3.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\"
-copy %~dp0src\slot4.cfg "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\"
-explorer "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+xcopy %~dp0src "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg" /S /E /Y
+explorer "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\Binds"
