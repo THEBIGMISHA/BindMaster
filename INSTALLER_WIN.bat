c07ab9acc459aa40c0250ed5c87bc0d7756e1b8a
Michael Nikolaev <esp12p@gmail.com>
2023-10-13 19:41:08 +0300

    12.10.23-2

--- INSTALLER_WIN.bat
+++ INSTALLER_WIN.bat
@@ -1,3 +1,3 @@
 @echo off
 xcopy %~dp0src "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg" /S /E /Y
-//explorer "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\Binds"
+explorer "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\BindMaster\Binds"
